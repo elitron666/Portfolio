c1212c7f15939050c741960c7ac391736b9e4040
elitron666 <elias.emtanes@burg-halle.de>
2024-06-14 13:42:08 +0200

    letzte

--- script.js
+++ script.js
@@ -3,7 +3,18 @@ var spruch = Array("Ey, Kollege, haste mal ne Ecke Tabak?",
 "Stell dich, du Flitzpiepe.",
 "Das letzte Bier gestern war wieder schlecht, du…",
 "Ey Hippieschwein. Mach’n Abflug. ",
-"Boar, hat wer ne Ibu?");
+"Boar, hat wer ne Ibu?", "Ey, sach ma Kollege, kenn wir uns nicht?", "Tschüss, ey!", "Bitte nicht zu nah mit die Pfoten an die Bilder ran, ne?");
+
+var raucherspruch = Array("Ich geh mal eine quarzen",
+  "Erst mal eene rochn",
+  "Ich geh mal zur Tanke, neue Ziesen holen, brauchste was?",
+  "Alter...genug gearbeitet, Zeit für ne Raucherpause", "Oh...ehm...ehm. Scheiße, die Herdplatte!");
+  
+
+  var wiederdaspruch = Array("das tat gut",
+    "holla, die Waldfee",
+    "Hab ich was verpasst?",
+    "Zurück an die Arbeit...seufz","solange man lebt, sollte man rauchen", "Boah, du, wird och nich billiger...");
 
 var spruchCounter = 0;
 var katziRaucht = false;
@@ -11,7 +22,8 @@ var katziRaucht = false;
 function katziHatGeraucht() {
   document.getElementById('katzi').style.display = 'flex';
   document.getElementById('randomSpruch').style.display = 'flex';
-  document.getElementById('randomSpruch').textContent = "bin wieder da";
+  var wiederda = wiederdaspruch[Math.floor(Math.random() * wiederdaspruch.length)];
+  document.getElementById('randomSpruch').textContent = wiederda;
   katziRaucht = false;
 }
 
@@ -29,7 +41,8 @@ function randomSpruch() {
     katziRaucht = true;
     setTimeout(katziWillRauchen, 5000);
     spruchCounter = 0;
-    document.getElementById('randomSpruch').textContent = "Tschau, bin rachn";
+    var quarzen = raucherspruch[Math.floor(Math.random() * raucherspruch.length)];
+    document.getElementById('randomSpruch').textContent = quarzen;
     return;
   }
 
@@ -55,6 +68,7 @@ function ShowCat() {
     randomSpruch();
     if (image)
       document.getElementById('randomSpruch').textContent = image.alt;
+    
   }
 }
 
@@ -68,6 +82,8 @@ span.onclick = function() {
     randomSpruch();
   }
   modal.style.display = "none";
+  document.getElementById('navigation').style.display = 'flex';
+
 }
 
 function initThumbnailClick() {
@@ -81,6 +97,8 @@ function initThumbnailClick() {
     img.onclick = function() {
       var katzi = document.getElementById('katzi');
       var spruch = document.getElementById('randomSpruch');
+      
+      document.getElementById('navigation').style.display = 'none';
 
       if (katzi.style.display === 'flex')
       {
@@ -107,4 +125,24 @@ function initThumbnailClick() {
   }
 }
 
-initThumbnailClick()
+
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navigation");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.scrollY >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+initThumbnailClick() 
